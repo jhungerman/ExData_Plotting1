a8cbae62c7f0eef849ab5e431c0bdae3dd94e3a8
jhungerman <joseph_hungerman@yahoo.com>
2018-05-06 10:11:21 -0500

    Updated 5/6/2018 10:11 am

--- plot1.R
+++ plot1.R
@@ -1,4 +1,11 @@
 #load packages
-
+#library(ggplot2)
+#library(dplyr)
+#library(tidyr)
 #get data from directory
-epc <- read.table("./ExpDataWk1/household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = c(as.Date("character", "%d/%m/%Y"),"NULL","numeric","numeric","numeric","numeric","numeric", "numeric","numeric"), stringsAsFactors = FALSE)
+epc <- read.table("household_power_consumption.txt", header = TRUE, sep = ";", na.strings = "?", colClasses = c("character","NULL","numeric","numeric","numeric","numeric","numeric", "numeric","numeric"), stringsAsFactors = FALSE)
+epc$Date <- strptime(epc$Date, "%d/%m/%Y")
+epc <- subset(epc, Date %in% c("2007-02-01","2007-02-02"))
+hist(epc$Global_active_power, main = "Global Active Power", xlab = "Global Active Power (kilowatts)", ylab = "Frequency", col="red")
+dev.copy(png, file="plot1.png", width=480, height=480)
+dev.off()
